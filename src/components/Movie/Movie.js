bf900a610d8851913a6374bddcf779f53d064f24
Arthur Molnar <molnar_arthur@yahoo.com>
2018-10-22 09:23:51 +0300

    Improve Movie component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -11,18 +11,70 @@ import './Movie.css';
 class Movie extends Component {
 
     state = {
+        movie: null,
+        actors: null,
+        directors: [],
+        loading: false
+    }
 
+    componentDidMount() {
+        this.setState({ loading: true })
+        // Fetch the movie
+        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result);
+                if (result.status_code) {
+                    this.setState({ loading: false });
+                } else {
+                    this.setState({ movie: result }, () => {
+                        // fetch actors
+                        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
+                        fetch(endpoint)
+                            .then(result => result.json())
+                            .then(result => {
+
+                            const directors = result.crew.filter( (member) => member.job === "Director");
+
+                            this.setState({
+                                actors: result.cast,
+                                directors,
+                                loading: false
+                            })
+                        })
+                    })
+                }
+            }).catch(error => console.error('Error:', error));
     }
 
     render() {
         return (
             <div className="rmdb-movie">
-                <Navigation />
-                <MovieInfo />
-                <MovieInfoBar />
-                <FourColGrid />
-                <Spinner />
-            </div>
+               {this.state.movie ? 
+                <div> 
+                    <Navigation movie={this.props.location.movieName} />
+                    <MovieInfo movie={this.state.movie} directors={this.state.directors} />
+                    <MovieInfoBar time={this.state.runtime} budget={this.state.movie.bidget} revenue={this.state.movie.revenue}/>
+                </div>
+                : null }
+                {this.state.actors ? 
+                <div className="rmdb-movie-grid">
+                    <FourColGrid header={'Actors'}>
+                        {this.state.actors.map( (element, i) => {
+                            return <Actor key={i} actor={element} />
+                        })}>
+                    </FourColGrid>
+                </div>
+                : null }
+                 {!this.state.actors && !this.state.loading ? <h1>No Movie Found!</h1> : null}
+                 {this.state.loading ? <Spinner /> : null}
+                }
+                </div>
         )
     }
 
